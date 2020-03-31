dbccd0e0c816c80fffac4039d0365d1c27e0743c
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-03-31 09:43:10 +0530

    Initial Setup
    Added Initial Project

---
+++ Timer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Mallikarjun on 31/03/20.
+//  Copyright © 2020 ADDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
